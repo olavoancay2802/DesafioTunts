c6e90eea57bc4cf4e37bfd1944b9d46cba0b72e1
olavoancay2802 <olavoancay2802@gmail.com>
2021-09-27 14:26:40 -0300

    Tirando mais partes desnecessárias do código

--- quickstart.py
+++ quickstart.py
@@ -7,7 +7,7 @@ from google.oauth2.credentials import Credentials
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 #ID sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1iWkXgDSz1sM7bkhewXJdSMBVJrmrZj9QRFLla8NMeuY'
+SPREADSHEET_ID = '1iWkXgDSz1sM7bkhewXJdSMBVJrmrZj9QRFLla8NMeuY'
 #Infos Range
 SPREADSHEET_INFOS = 'engenharia_de_software!A4:F'
 #Semester classes range
@@ -15,32 +15,18 @@ TOTAL_CLASSES = 'engenharia_de_software!A2'
 
 def main():
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
 
     service = build('sheets', 'v4', credentials=creds)
 
     # Call the Sheets API
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
                                 range=SPREADSHEET_INFOS).execute()
     values = result.get('values', [])
     
-    totalClasses = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+    totalClasses = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
                                 range=TOTAL_CLASSES).execute()
     valuesTotalClasses = totalClasses.get('values')
     
@@ -51,8 +37,8 @@ def main():
     numbers = ""
 
     for letter in txt:
-       if letter.isdigit():
-          numbers += str(letter)
+        if letter.isdigit():
+            numbers += str(letter)
           
     numberSemesterClasses = int(numbers)
 
@@ -83,7 +69,7 @@ def main():
             auxUpdate.append(finalScoreNeeded)
             update.append(auxUpdate)
         
-        request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+        request = sheet.values().update(spreadsheetId=SPREADSHEET_ID,
                                                              range='engenharia_de_software!G4', valueInputOption='RAW', body={'values':update}).execute()
 
 if __name__ == '__main__':
